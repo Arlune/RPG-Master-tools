5bd97ef0cdcccf381f96a8c65c223bb4680fd579
cbonaudo <cbonaudo@hotmail.fr>
2020-02-08 23:41:33 +0100

    Updates

---
+++ precache-manifest.9281e0d4c789b4d47ce8e652364fed3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1833a6f3fc6a7e0611372f0d8c52a1a2",
+    "url": "/rpg-master-tools/index.html"
+  },
+  {
+    "revision": "c75c00ec24a2353c1ff7",
+    "url": "/rpg-master-tools/static/css/main.dfe69c4f.chunk.css"
+  },
+  {
+    "revision": "f83d4b951e2abcf0081a",
+    "url": "/rpg-master-tools/static/js/2.a8b12b0d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rpg-master-tools/static/js/2.a8b12b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75c00ec24a2353c1ff7",
+    "url": "/rpg-master-tools/static/js/main.5e5ec710.chunk.js"
+  },
+  {
+    "revision": "091c45fa13cf4eeb89b5",
+    "url": "/rpg-master-tools/static/js/runtime-main.2134068f.js"
+  },
+  {
+    "revision": "00c8e4b0297bdb103d2e3ead977dd5bf",
+    "url": "/rpg-master-tools/static/media/BladeRunner.00c8e4b0.TTF"
+  },
+  {
+    "revision": "82987827f2526914f03f1488ebda55a2",
+    "url": "/rpg-master-tools/static/media/Nasalization.82987827.ttf"
+  }
+]);
